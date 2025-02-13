4bba603dbdf36e166295054e38ca94aa68b5eced
Imil Ametov <charlz_klug@mail.ru>
2025-02-13 10:19:30 +0300

    Solved exercise 01.

---
+++ 01.5/Exercises/01_exercise/main.cpp
@@ -0,0 +1,10 @@
+// Change the program to output the two lines
+// Hello, programming!
+// Here we go!
+// clang++-18 -std=c++20 main.cpp
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, programming!\nHere we go!\n";
+    return (0);
+}
