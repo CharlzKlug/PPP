8b916d9e206a105bcfaaf8059226ed0313518397
Imil Ametov <charlz_klug@mail.ru>
2025-02-21 15:21:59 +0300

    Drill 01 solved

---
+++ 02/Drills/01/main.cpp
@@ -0,0 +1,8 @@
+#include "PPP.h"
+
+int main() {
+    cout << "Enter the name of the person you want to write to: ";
+    string first_name;
+    cin >> first_name;
+    cout << "Dear " << first_name << ", ...\n";
+}
