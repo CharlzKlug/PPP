89db3f082237225fce853f830a9b2264cdac8d9b
Imil Ametov <charlz_klug@mail.ru>
2025-05-12 17:11:58 +0300

    Exercise 18 solved

---
+++ 03/Exercises/18_exercise/main.cpp
@@ -0,0 +1,44 @@
+#include "../../../PPP.h"
+
+bool presentP(vector<string>*, string);
+void printLists(vector<string>*, vector<int>*);
+
+int main() {
+    vector<string> names;
+    vector<int> scores;
+
+    string name;
+    int score;
+    while (true) {
+	cout << "Enter name and score [name score] or [NoName 0] for exit: ";
+	cin >> name;
+	cin >> score;
+	if (name == "NoName" && score == 0) {break;}
+	if (presentP(&names, name)) {
+	    cout << "This name already present!\n"; continue;
+	}
+	names.push_back(name);
+	scores.push_back(score);
+    }
+    printLists(&names, &scores);
+}
+
+bool presentP(vector<string>* inputVector, string element) {
+    if (count(inputVector->begin(), inputVector->end(), element) != 0) {
+	return true;
+    }
+    return false;
+}
+
+void printLists(vector<string>* inputNames, vector<int>* inputScores) {
+    size_t namesSize = inputNames->size();
+    size_t scoresSize = inputScores->size();
+    for (size_t i= 0; i < namesSize || i < scoresSize; i++) {
+	if (i < namesSize) {cout << (*inputNames)[i];} else
+	    {cout << "[No name]";}
+	cout << ' ';
+	if (i < scoresSize) {cout << (*inputScores)[i];} else
+	    {cout << "[No score]";}
+	cout << '\n';
+    }
+}
--- PPP.h
+++ PPP.h
@@ -4,6 +4,8 @@
 #include <vector>
 #include <ranges>
 #include <algorithm>
+#include <map>
+
 using namespace std;
 
 // write ``error: s'' and exit program
