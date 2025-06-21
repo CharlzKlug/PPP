17a179ad42fdfeaae79383bf4263641e412f0db4
Ametov Imil Ilimdarovich <charlz_klug@mail.ru>
2025-06-21 22:32:47 +0300

    Drill

---
+++ 04/123_drill/main.cpp
@@ -0,0 +1,14 @@
+#include "../../PPP.h"
+
+int main()
+  try {
+    string s = "ape"; if (s=="fool") cout << "Success!\n";
+    return 0;
+  }
+  catch (exception& e) {
+    cerr << "error: " << e.what() << '\n';
+    return 1;
+  } catch(...) {
+    cerr << "Oops: unknown exception!\n";
+    return 2;
+  }
